63762e76cf34e89d5da024a73c3dcc34d00cd9b5
Theo Andresier <49570057+Threpio@users.noreply.github.com>
2024-09-28 18:29:21 +0200

    Schema update

--- schema.sql
+++ schema.sql
@@ -2,4 +2,10 @@ CREATE TABLE IF NOT EXISTS data_instance (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
     value jsonb NOT NULL
-)
+);
+
+CREATE TABLE IF NOT EXISTS pilots (
+    cid BIGINT PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    value jsonb
+);
